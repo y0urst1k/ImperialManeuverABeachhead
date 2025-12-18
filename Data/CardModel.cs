337cb349179d670d8e3a446570aa7c6d8e2002de
y0urst1k <y0urstyurk@yandex.ru>
2025-12-18 16:54:25 +0300

    feat: adding Models

---
+++ Data/CardModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Data
+{
+    public class CardModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } // Историческая справка
+        public string ImagePath { get; set; }
+
+        public int Cost { get; set; }      // Стоимость разыгрывания
+        public int Attack { get; set; }    // Атака
+        public int Health { get; set; }    // Здоровье
+        public int Production { get; set; } // Сколько ресурсов дает (для тыловых карт)
+
+        public CardType Type { get; set; }
+
+        // Свойства для игры (изменяемые)
+        public bool IsExhausted { get; set; } // Ходил ли в этом ходу
+    }
+}
---
+++ Data/PlayerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Data
+{
+    public class PlayerModel
+    {
+        public string Name { get; set; }
+        public int Health { get; set; } = 30;
+        public int CurrentResources { get; set; } = 0;
+    }
+}
